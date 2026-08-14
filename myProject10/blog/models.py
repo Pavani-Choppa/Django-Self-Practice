abe533b78eb9bd0b84d59a5ebf5f79be68c6cddc
Pavani-Choppa <pavanicm011@gmail.com>
2026-08-14 18:07:16 +0530

    Retreving The Details from the Database by using shell

--- myProject10/blog/models.py
+++ myProject10/blog/models.py
@@ -5,4 +5,9 @@ class Student(models.Model):
     name = models.CharField(max_length = 100)
     age = models.IntegerField()
     emmail = models.EmailField(max_length=255, unique=True)
+    city = models.CharField(default="Unknown",max_length = 100)
+
+    def __str__(self):
+        return self.name
+    
 
