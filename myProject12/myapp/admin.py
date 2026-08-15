36a86f68db2e4d3a2f2eb1be5b8a7cc6d5242dfa
Pavani-Choppa <pavanicm011@gmail.com>
2026-08-15 17:30:45 +0530

    Practiced How to Register Models in Admin.py

--- myProject12/myapp/admin.py
+++ myProject12/myapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from myapp.models import Student,Profile
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Profile)
--- myProject12/myapp/models.py
+++ myProject12/myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    city = models.CharField()
+
+    def __str__(self):
+        return str(self.city)
+
+class Profile(models.Model):
+    bio = models.TextField()
+    location = models.CharField(max_length=100)
+    dob = models.DateField(null=True,blank=True)
+
+    def __str__(self):
+        return str(self.dob)
